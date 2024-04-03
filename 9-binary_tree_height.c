ba1a364dd475b6d5a63e3e5d2bc5161baa023b72
Idongesit Ekanem <aidisapp94@gmail.com>
2024-04-03 12:59:25 +0100

    Task 9 Solution fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,18 +6,26 @@
  *
  * tree to measure the height
  *
- * Return: Height of the tree, 0 if tree is NULL
+ * Return: will be height of the tree, 0 if tree is NULL
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL)
-        return (0);
+	if (tree)
+	{
+		left_height = 0;
+		right_height = 0;
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+		if (tree->left)
+			left_height = 1 + binary_tree_height(tree->left);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+		if (tree->right)
+			right_height = 1 + binary_tree_height(tree->right);
+
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+
+	return (0);
 }
