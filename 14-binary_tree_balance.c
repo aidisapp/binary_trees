f3b15517d249bf0b6034154cd42bef7c244b31ce
Idongesit Ekanem <aidisapp94@gmail.com>
2024-04-03 23:58:55 +0100

    Task 14 Solution

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - this function measures the balance factor
- * of a binary tree
- * @tree: this is the pointer to the root node of the tree to measure the balance factor
+ * binary_tree_balance - measures the balance factor
  *
- * Return: this is the balance factor of the tree, 0 if tree is NULL
+ * of a binary tree
+ *
+ * @tree: this is the pointer to the root node
+ * of the tree to measure the balance factor
+ *
+ * Return: Balance factor of the tree, 0 if tree is NULL
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
