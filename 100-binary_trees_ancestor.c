9050a80564c6dbeec4a8862ced2b3532fdd75828
Idongesit Ekanem <aidisapp94@gmail.com>
2024-04-07 22:16:24 +0100

    Betty error fix

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,7 +9,7 @@
  */
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-                                     const binary_tree_t *second)
+					const binary_tree_t *second)
 {
 	binary_tree_t *mother, *father;
 
